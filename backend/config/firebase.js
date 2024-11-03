b15edf4dc30a305a592d51093c37b4292770d0d0
Farh4nHanz <f620031718@gmail.com>
2024-11-03 13:29:03 +0700

    Changed firebase config file

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,15 +1,6 @@
 import admin from "firebase-admin";
-import { readFileSync } from "fs";
-import path from "path";
 
-const __dirname = path.resolve();
-
-const serviceAccount = JSON.parse(
-  readFileSync(
-    path.join(__dirname, "serviceAccountKey.json"),
-    "utf-8"
-  )
-);
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
